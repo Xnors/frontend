1905b157ababa62f8152dc9e624128d48ad19d29
fexcode <2734664632@qq.com>
2026-02-13 15:15:22 +0800

    ...我已无力写commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/apis",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://120.55.38.133:8000/apis",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
